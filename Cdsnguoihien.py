f3ab227d043c1b1403889a83b78b9df2ebc95ccf
tbcv-ui <thienbinhchauvu@gmail.com>
2026-03-08 11:37:41 +0700

    Update Cdsnguoihien.py

--- Cdsnguoihien.py
+++ Cdsnguoihien.py
@@ -1,17 +1,12 @@
-# Tên file: ds_nguoi_hien.py
-
 class BloodBank:
-    """Lớp quản lý danh sách và các thuật toán điều phối"""
+    """Lớp quản lý danh sách người hiến máu"""
     def __init__(self):
-        self.donors_list = [] # Cấu trúc dữ liệu chính
+        self.donors_list = [] # Cấu trúc dữ liệu List
 
     def add_donor(self, donor):
+        """Thêm người hiến vào danh sách"""
         self.donors_list.append(donor)
 
-    def search_donors(self, target_blood_type, target_city):
-        """Thuật toán lọc người theo nhóm máu và khu vực"""
-        results = []
-        for donor in self.donors_list:
-            if donor.blood_type == target_blood_type and donor.city == target_city:
-                results.append(donor)
-        return results
+    def get_all_donors(self):
+        """Lấy toàn bộ danh sách hiện có"""
+        return self.donors_list
