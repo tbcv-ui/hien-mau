e828f492541200f5599c321cd6f5f73fef08a981
tbcv-ui <thienbinhchauvu@gmail.com>
2026-03-08 11:25:26 +0700

    Create tab1.py
    cập nhật tab1

---
+++ tab1.py
@@ -0,0 +1,13 @@
+class Donor:
+    """Lớp đại diện cho một người hiến máu"""
+
+    def __init__(self, ten, gioitinh, tuoi, cannang, nhommau, noisong, sdt, benhnen):
+        self.ten = ten
+        self.gioitinh = gioitinh
+        self.tuoi = tuoi
+        self.cannang = cannang
+        self.nhommau = nhommau
+        self.noisong = noisong
+        self.sdt = sdt
+        self.benhnen = benhnen
+
